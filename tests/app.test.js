d5523fdafd07d3946cb7feec1eb8554aff0f2d02
Shalommoise <mshalom689@gmail.com>
2021-02-23 18:01:17 +0000

    testing suite

---
+++ tests/app.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../app");
+
+
+describe("/api", ()=>{
+  describe("/games", ()=>{
+    test('201 POST game', () => {
+      
+    });
+  })
+})
