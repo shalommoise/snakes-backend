be51f8da39ceb30dfc7effa909bfe10b9562d04a
Shalommoise <mshalom689@gmail.com>
2021-02-24 13:22:31 +0000

    first test

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,7 +5,14 @@ const app = require("../app");
 describe("/api", ()=>{
   describe("/games", ()=>{
     test('201 POST game', () => {
-      
+      return request(app)
+      .post("/api/games/")
+      .send({player1: "Sam"})
+      // .expect(201)
+      .then((res)=>{
+       console.log(res.body)
+        expect(res.body.game.player1).toBe("Sam");
+      })
     });
   })
 })
