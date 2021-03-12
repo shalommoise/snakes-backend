d60503585c4fde4aa676a9a148b7881346a95b04
Shalommoise <mshalom689@gmail.com>
2021-03-12 13:34:22 +0000

    removed comments

--- utils/utils.js
+++ utils/utils.js
@@ -19,34 +19,6 @@ const radnomCoordinate = (size)=>{
    if(y === 0) y++;
    return [x,y]
 }
-
-// const isSnakeEatingSnake = (snake1, snake2) =>{
-//   if(typeof snake1 !== "object" || typeof snake2 !== "object") return {newSnake1:[],newSnake2:[]};
-//     let body1 = [...snake1];
-//     let body2 = [...snake2];
-//     let head1 = body1.shift();
-//     let head2 = body2.shift();
-//     const [x1, y1] = head1;
-//     const [x2, y2] = head2;
-//     let index1 = null;
-//     let index2 = null;
-//     let lose1 = false;
-//     let lose2 = false;
-//   body2.forEach((coordinate, i) => { 
-//     if(coordinate[0] === x1 && coordinate[1] === y1) index2 = i;
-//     if(coordinate[0] === x2 && coordinate[1] === y2) lose2 = true;
-//   })
-//   body1.forEach((coordinate, i) => { 
-//     if(coordinate[0] === x2 && coordinate[1] === y2) index1 = i;
-//     if(coordinate[0] === x1 && coordinate[1] === y1) lose1 = true;
-//   })
-//   body2 = index2 || index2 === 0 ? body2.slice(0,index2) : body2;
-//   body1 = index1 || index1=== 0 ? body1.slice(0,index1) : body1;
-//   if(lose1) {head1 = [] ; body1 = []};
-//   const newSnake1 = lose1 ? [] : [head1, ...body1];
-//   const newSnake2 = lose2 ? [] : [head2, ...body2];
-//     return {newSnake1, newSnake2}
-// }
 const separateSnake = (snake)=>{
   const body = [...snake];
   const head = body.shift();
