47a88a2a146d9c31824d0d5c956d69aa5f992f33
Shalommoise <mshalom689@gmail.com>
2021-03-18 11:20:48 +0000

    heroku deploy

--- Schema/index.js
+++ Schema/index.js
@@ -1,10 +1,15 @@
 let mongoose = require('mongoose');
-
+const {MONGODB_URI} = require('../mongodbURI')
 mongoose.connect(
-    process.env.MONGODB_URI ||
-    'mongodb://localhost/snakes-be'
-);
-
+        MONGODB_URI ||
+    'mongodb://localhost/snakes-be',
+     { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+mongoose.connection.on('connected', ()=>{
+    console.log("Horray, mongoose is connected")
+})
 let Game = require("./game");
 
 module.exports.Game = Game; 
+
