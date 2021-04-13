797bf2ba26d7c15009b350d1b4cbede34d17db72
Shalommoise <mshalom689@gmail.com>
2021-04-13 21:59:22 +0100

    added timeout

--- Schema/index.js
+++ Schema/index.js
@@ -3,9 +3,9 @@ const {MONGODB_URI} = require('../mongodbURI')
 mongoose.connect(
         MONGODB_URI ||
     'mongodb://localhost/snakes-be',
-     { useNewUrlParser: true, useUnifiedTopology: true }
+     { useNewUrlParser: true, useUnifiedTopology: true , serverSelectionTimeoutMS: 5000}
 ).then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err.reason));
 mongoose.connection.on('connected', ()=>{
     console.log("Horray, mongoose is connected")
 })
