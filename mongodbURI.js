21b93c6572cf5090c34ccda16d9493e278412eb7
Shalommoise <mshalom689@gmail.com>
2021-04-13 22:19:27 +0100

    changed string

--- mongodbURI.js
+++ mongodbURI.js
@@ -1,4 +1,4 @@
 const userName = 'Shalommoise';
 const password = 'LmSY6vqFnWG79EP'
 
-const MONGODB_URI = `mongodb+srv:/${userName}:${password}@snakes-be.bmn12.mongodb.net/snakes-be?retryWrites=true&w=majority` 
+const MONGODB_URI = `mongodb+srv://Shalommoise:${password}@snakes-be.bmn12.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` 
