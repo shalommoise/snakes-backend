e089c1e5685162c33a1ecca46cc918c7cd416324
Shalommoise <mshalom689@gmail.com>
2021-04-13 22:23:44 +0100

    fixed string

--- mongodbURI.js
+++ mongodbURI.js
@@ -1,4 +1,4 @@
 const userName = 'Shalommoise';
 const password = 'LmSY6vqFnWG79EP'
 
-const MONGODB_URI = `mongodb+srv://Shalommoise:${password}@snakes-be.bmn12.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` 
+exports.MONGODB_URI = `mongodb+srv://Shalommoise:${password}@snakes-be.bmn12.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` 
